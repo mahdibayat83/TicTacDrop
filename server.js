fb0d98e51d5bbe187a5f4c6dfea37b7ae0253dc4
Caspian <caspianrz@gmail.com>
2026-05-26 11:58:35 +0330

    add random hashed rooms
    Signed-off-by: Caspian <caspianrz@gmail.com>

--- server.js
+++ server.js
@@ -2,38 +2,47 @@ import express from 'express';
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 import cors from 'cors';
+import crypto from 'node:crypto';
 
 const port = 9010;
 const app = express();
 
 var corsOptions = {
-  origin: 'localhost:3000',
-  optionsSuccessStatus: 200 
+	origin: 'localhost:3000',
+	optionsSuccessStatus: 200
 }
 
 app.use(cors(corsOptions));
 
 const httpServer = createServer(app);
+
 const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
+	cors: {
+		origin: "http://localhost*",
+		methods: ["GET", "POST"],
+		credentials: true,
+	},
 });
+
+const free_rooms = [];
 
 io.on("connection", (socket) => {
 	console.log("connected:", socket.id);
 
-	socket.on("join", (room = "global") => {
-		socket.join(room);
-		const clients = io.sockets.adapter.rooms.get(room);
+	socket.on("join", () => {
+		let room_id = free_rooms.pop();
+		if (room_id == undefined) {
+			room_id = crypto.randomBytes(20).toString('hex');
+			free_rooms.push(room);
+		}
+		socket.join(room_id);
+		const clients = io.sockets.adapter.rooms.get(room_id);
 		const count = clients ? clients.size : 0;
 		let assigned = "spectator";
 		if (count === 1) assigned = "X";
 		else if (count === 2) assigned = "O";
 		socket.emit("player-assign", assigned);
-		io.to(room).emit("room-update", { count });
+		io.to(room_id).emit("room-update", { count, room_id });
 	});
 
 	socket.on("move", (data) => {
